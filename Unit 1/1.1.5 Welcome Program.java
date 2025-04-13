b65882a551a6671526aa29c98c897975fd308733
Ivan Belousov <info@ivanbelousov.com>
2025-04-14 00:20:08 +0800

    Create 1.1.5 Welcome Program.java

---
+++ Unit 1/1.1.5 Welcome Program.java
@@ -0,0 +1,10 @@
+public class Welcome 
+{
+    public static void main(String[] args)
+    {
+       
+       System.out.println("My name is Ivan");
+       System.out.println("My favorite hobby is playing tennis");
+       
+    } 
+}
