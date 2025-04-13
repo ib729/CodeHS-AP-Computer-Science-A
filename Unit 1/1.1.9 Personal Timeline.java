ddb4dc828f238d696250c8c246b04bdbcb51fb7c
Ivan Belousov <info@ivanbelousov.com>
2025-04-14 00:21:59 +0800

    Create 1.1.9 Personal Timeline.java

---
+++ Unit 1/1.1.9 Personal Timeline.java
@@ -0,0 +1,31 @@
+public class Timeline
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Age\t| M/YY - Description");
+        System.out.println("8 \t| 12/1 - Went to Dubai");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("10\t| 3/22 - Dr Disrespect got caught");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("12\t| 5/3 - Alergic to shrimp now for some reason");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("14\t| 7/29 - Twitch Con");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("16\t| 5/28 - Nickocado Avacado");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.print("18\t| 7/3 - Skibidi toilet");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+        System.out.println("  \t|                       ");
+    }
+}
